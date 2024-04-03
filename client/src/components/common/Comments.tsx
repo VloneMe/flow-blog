14008f2836066523e42ecb0261084c59f3ae7dc4
vlone <vlone@localhost.localdomain>
2024-04-03 08:11:48 -0400

    Client updates

---
+++ client/src/components/common/Comments.tsx
@@ -0,0 +1,158 @@
+import { useState, useEffect, useContext } from 'react';
+import { Input } from '../ui/input';
+import { Button } from '../ui/button';
+import { UserContext } from '@/context/UserContext';
+import { MdDeleteOutline } from "react-icons/md";
+
+interface Comment {
+    _id: string;
+    content: string;
+    author: {
+        username: string;
+    };
+    likes: number;
+    postId: string;
+}
+
+interface User {
+    _id: string;
+    username: string;
+}
+
+interface Props {
+    postId?: string;
+    postInfo?: any
+}
+
+const Comments = ({ postId, postInfo }: Props) => {
+    const [comments, setComments] = useState<Comment[]>([]);
+    const [newComment, setNewComment] = useState<string>('');
+    const [users, setUsers] = useState<User[]>([]);
+    const [error, setError] = useState<string | null>(null);
+    const { userInfo } = useContext(UserContext);
+
+    useEffect(() => {
+        if (postId) {
+            fetchComments(postId);
+            fetchUsers();
+        }
+    }, [postId]);
+
+    const fetchComments = async (postId: string) => {
+            await fetch(`http://localhost:5000/api/comments/${postId}`)
+            .then(res => {
+                if (!res.ok) {
+                    throw new Error('Failed to fetch comments!');
+                }
+                return res.json();
+            })
+            .then(postInfo => setComments(postInfo))
+            .catch(err => {
+                setError(err.message);
+            });
+        };
+
+    const fetchUsers = async () => {
+        try {
+            const res = await fetch(`http://localhost:5000/api/blogposts/`);
+            if (!res.ok) {
+                throw new Error('Failed to fetch users!');
+            }
+            const data = await res.json();
+            setUsers(data);
+        } catch (err) {
+            setError(err?.message);
+        }
+    };
+
+    const handleAddComment = async () => {
+        try {
+            const res = await fetch(`http://localhost:5000/api/comments/${postId}`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    postId,
+                    author: userInfo?.id,
+                    content: newComment 
+                    }),
+                credentials: 'include'
+            });
+            if (res.ok) {
+                const newCommentData: Comment = await res.json();
+                setComments([...comments, newCommentData]);
+                setNewComment('');
+            } else {
+                console.error('Failed to add comment');
+            }
+        } catch (error) {
+            console.error('Error adding comment:', error);
+        }
+    };
+
+    const handleDeleteComment = async (commentId: string) => {
+        try {
+            const res = await fetch(`http://localhost:5000/api/comments/${commentId}`, {
+                method: 'DELETE'
+            });
+            if (res.ok) {
+                setComments(comments.filter(comment => comment._id !== commentId));
+            } else {
+                console.error('Failed to delete comment');
+            }
+        } catch (error) {
+            console.error('Error deleting comment:', error);
+        }
+    };
+
+    return (
+        <div className="comments-section">
+            <h2 className="text-lg font-bold mb-4">Comments</h2>
+            <div className="comment-input flex mb-4">
+                <Input
+                    type="text"
+                    value={newComment}
+                    onChange={(e) => setNewComment(e.target.value)}
+                    className="border rounded py-2 px-3 mr-2 w-full"
+                    placeholder="Add a comment..."
+                />
+                <Button onClick={handleAddComment} className="text-white py-3 px-5 rounded">
+                    Add
+                </Button>
+            </div>
+            <ul>
+                {comments.map(comment => (
+                    // Only display comments with matching postId
+                    comment.post === postId &&
+                    <li key={comment._id} className="mb-4">
+                        <p className="text-xl font-bold">{comment.content}</p>
+
+                        <div className='flex items-center gap-5 text-[1rem] font-mono pl-3'
+                        >
+                       
+                            <p className="text-gray-500">
+                                By: {comment.author.username || 'Anonymous'}
+                            </p>
+
+                            
+                            
+                            {userInfo?.id === postInfo.author._id && comment._id &&
+                            <>
+                                <p>|</p>
+                                <div className="mt-2">
+                                    <button onClick={() => handleDeleteComment(comment._id)}        className="hover:text-red-700">
+                                        <MdDeleteOutline />
+                                    </button>
+                                </div>
+                            </>
+                            }
+                        </div>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default Comments;
--- client/src/pages/SinglePostPage.tsx
+++ client/src/pages/SinglePostPage.tsx
@@ -1,125 +1,108 @@
-import { Container } from "@/components/Container";
-import { Button } from "@/components/ui/button";
-import { useContext, useEffect, useState } from "react";
-import { format } from "date-fns";
-import { Link, Navigate, useParams } from "react-router-dom"
-import { FaEdit } from "react-icons/fa";
-import { MdOutlineDeleteOutline } from "react-icons/md";
-import { UserContext } from "@/context/UserContext";
+import React, { useContext, useEffect, useState } from 'react';
+import { Container } from '@/components/Container';
+import { Button } from '@/components/ui/button';
+import { format } from 'date-fns';
+import { Link, Navigate, useParams } from 'react-router-dom';
+import { FaEdit } from 'react-icons/fa';
+import { MdOutlineDeleteOutline } from 'react-icons/md';
+import { UserContext } from '@/context/UserContext';
+import Comments from '@/components/common/Comments';
 
-
-export const SinglePostPage = () => {
-
-    const { id } = useParams();
-    const [postInfo, setPostInfo] = useState(null);
+export const SinglePostPage: React.FC = () => {
+    const { id } = useParams<{ id: string }>();
+    const [postInfo, setPostInfo] = useState<any>(null);
     const { userInfo } = useContext(UserContext);
-    const [deleted, setDeleted] = useState(false);
-    const [display, setDisplay] = useState(false);
-    
+    const [deleted, setDeleted] = useState<boolean>(false);
+    const [display, setDisplay] = useState<boolean>(false);
+    const [error, setError] = useState<string | null>(null);
 
     useEffect(() => {
-        fetch(`http://localhost:5000/api/blogposts/${id}`)
-        .then(res => {
-            res.json()
-            .then(postInfo => {
-                setPostInfo(postInfo);
-            })
-        })
-    }, []);
+        if (id) {
+            fetch(`http://localhost:5000/api/blogposts/${id}`)
+                .then(res => {
+                    if (!res.ok) {
+                        throw new Error('Failed to fetch post');
+                    }
+                    return res.json();
+                })
+                .then(postInfo => setPostInfo(postInfo))
+                .catch(err => {
+                    setError(err.message);
+                });
+        }
+    }, [id]);
 
     const deletePost = async () => {
-        const res = await fetch(`http://localhost:5000/api/blogposts/${id}`, {
-            method: 'DELETE',
-        });
-
-        if (res.ok){
-            alert("Post Succesfully Deleted!")
-            setDeleted(true);
+        try {
+            const res = await fetch(`http://localhost:5000/api/blogposts/${id}`, {
+                method: 'DELETE',
+            });
+            if (res.ok) {
+                alert('Post Successfully Deleted!');
+                setDeleted(true);
+            } else {
+                throw new Error('Failed to delete post');
+            }
+        } catch (err: any) {
+            setError(err.message);
         }
+    };
+
+    if (!postInfo) {
+        if (error) {
+            return <p>Error: {error}</p>;
+        }
+        return null;
     }
 
-    if (!postInfo) return "";
-
-    // for None User edit access
     if ((!(userInfo && userInfo.id) === postInfo.author._id)) {
         setDisplay(true);
         return <Navigate to={'/'} />;
-        useEffect(() => {
-            window.location.reload();
-        }, [])
     }
 
-    if (deleted){
-        return <Navigate to={'/'} />
-        useEffect(() => {
-            window.location.reload();
-        }, [])
+    if (deleted) {
+        return <Navigate to={'/'} />;
     }
 
     return (
-        <section className="my-24"
-        >
-            <Container className="space-y-8 font-serif"
-            >
-                <div className="space-y-5"
-                >
-                    <h1 className="text-5xl font-bold tracking-wider"
-                    >{postInfo.title}</h1>
-
-                    <div className={`flex border-b-2 items-center  text-gray-600 text-lg
-                                    justify-between`} 
-                    >
-                        <div className={`flex items-center ${display ? 'justify-between': 'gap-5'}`}
-                        >
-                            <time>{format(new Date(postInfo.createdAt), 'MMM d, yyyy HH:mm')}</time> 
+        <section className="my-24">
+            <Container className="space-y-8 font-serif">
+                <div className="space-y-5">
+                    <h1 className="text-5xl font-bold tracking-wider">{postInfo.title}</h1>
+                    <div className={`flex border-b-2 items-center text-gray-600 text-lg justify-between`}>
+                        <div className={`flex items-center ${display ? 'justify-between' : 'gap-5'}`}>
+                            <time>{format(new Date(postInfo.createdAt), 'MMM d, yyyy HH:mm')}</time>
                             {!display && <span>|</span>}
-                            <p className="capitalize"
-                            >By: {postInfo.author.username}</p>
+                            <p className="capitalize">By: {postInfo.author.username}</p>
                         </div>
-
-                        <div className="flex items-center gap-5"
-                        >
-
-                            {userInfo.id === postInfo.author._id && (
-                                <div className="flex gap-5"
-                                >
-                                    <Link to={`/edit/${postInfo._id}`}
-                                            className=""
-                                    >
-                                        <Button className="text-lg flex items-center gap-x-5 px-5 py-5 text-right"
-                                        > <FaEdit /> Edit Post</Button>
+                        <div className="flex items-center gap-5">
+                            {userInfo?.id === postInfo.author._id && (
+                                <div className="flex gap-5">
+                                    <Link to={`/edit/${postInfo._id}`} className="">
+                                        <Button className="text-lg flex items-center gap-x-5 px-5 py-5 text-right">
+                                            <FaEdit /> Edit Post
+                                        </Button>
                                     </Link>
-
-                                    
-                                    <Button     onClick={deletePost}
-                                                className="text-lg bg-red-800 hover:bg-red-600 flex items-center gap-x-5 px-5 py-5 text-right"
-                                    > <MdOutlineDeleteOutline /> Delete Post</Button>
-                                    
+                                    <Button onClick={deletePost} className="text-lg bg-red-800 hover:bg-red-600 flex items-center gap-x-5 px-5 py-5 text-right">
+                                        <MdOutlineDeleteOutline /> Delete Post
+                                    </Button>
                                 </div>
                             )}
                         </div>
                     </div>
                 </div>
-                <div className="w-full"
-                >
-                    <img    src={`http://localhost:5000/${postInfo.cover}`} 
-                            className="w-full max-h-[500px] bg-cover object-cover"
-                    />
+                <div className="w-full">
+                    <img src={`http://localhost:5000/${postInfo.cover}`} className="w-full max-h-[500px] bg-cover object-cover" />
                 </div>
-
-                <div className="w-full bg-slate-600/50 border-l-8 border-green-700 rounded-md"
-                >
-                    <p className="text-2xl font-mono p-10 tracking-wide"
-                    >{'"'+postInfo.summary}</p>
+                <div className="w-full bg-slate-600/50 border-l-8 border-green-700 rounded-md">
+                    <p className="text-2xl font-mono p-10 tracking-wide">{'"' + postInfo.summary}</p>
                 </div>
-
-                <div className="space-y-8"
-                >
-                    <div    dangerouslySetInnerHTML={{__html: postInfo.content}}
-                            className="text-2xl"
-                    />
+                <div className="space-y-8">
+                    <div dangerouslySetInnerHTML={{ __html: postInfo.content }} className="text-2xl" />
                 </div>
+                <Comments postId={id} postInfo={postInfo} 
+                />
             </Container>
         </section>
-    )
-}
+    );
+};
